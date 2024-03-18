c6b58a1fd9b2919bada23b17e2a4216083e8d386
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-03-18 22:38:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
+  res.sendStatus(200);
 });
 
 // Define schema for Arduino data
