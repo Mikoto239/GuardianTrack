67bc9acf2259afee1951085bf94d1e13fedd5dc6
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-03-18 17:35:27 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Define your routes for the home page
+// Define routes for the home page
 router.get('/', (req, res) => {
   res.send('Welcome to the home page!');
 });
 
-module.exports = router; 
+module.exports = router;
